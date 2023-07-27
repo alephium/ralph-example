9351472d104e1f1042fb4aa94f36d27939584e48
lbqds <lbqds@outlook.com>
2023-07-27 13:58:16 +0800

    Add simple-auction example

---
+++ simple-auction/test/auction.test.ts
@@ -0,0 +1,123 @@
+import {
+  ALPH_TOKEN_ID,
+  Address,
+  ONE_ALPH,
+  addressFromContractId,
+  groupOfAddress,
+  sleep,
+  subContractId
+} from '@alephium/web3'
+import { Auction, AuctionInstance, Bidder } from '../artifacts/ts'
+import {
+  alph,
+  auctionEnd,
+  auctionEndFailed,
+  balanceOf,
+  bid,
+  bidFailed,
+  deployAuction,
+  randomP2PKHAddress,
+  transferAlphTo,
+  withdraw,
+  withdrawFailed
+} from './utils'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { testAddress } from '@alephium/web3-test'
+import * as base58 from 'bs58'
+
+describe('test auction', () => {
+  const groupIndex = groupOfAddress(testAddress)
+  const auctionDuration = 10 * 1000 // 10s
+
+  let auction: AuctionInstance
+  let auctioneer: Address
+  let beneficiaryAsset: string
+  let beneficiaryAssetAmount: bigint
+  let bidders: PrivateKeyWallet[]
+
+  beforeEach(async () => {
+    const auctionEndTime = Date.now() + auctionDuration
+    auctioneer = randomP2PKHAddress(groupIndex)
+    auction = (await deployAuction(auctioneer, auctionEndTime)).contractInstance
+    const auctionState = await auction.fetchState()
+    beneficiaryAsset = auctionState.fields.beneficiaryAsset
+    beneficiaryAssetAmount = auctionState.fields.beneficiaryAssetAmount
+    bidders = Array.from(Array(2).keys()).map((_) => PrivateKeyWallet.Random(groupIndex))
+    for (const bidder of bidders) {
+      await transferAlphTo(bidder.address, alph(1000))
+    }
+  })
+
+  async function checkHighestBidder(address: Address, amount: bigint) {
+    const state = await auction.fetchState()
+    expect(state.fields.highestBidder).toEqual(address)
+    expect(state.fields.highestBid).toEqual(amount)
+  }
+
+  async function checkBidderState(bidder: PrivateKeyWallet, amount: bigint) {
+    const path = base58.decode(bidder.address)
+    const bidderContractId = subContractId(auction.contractId, path, groupIndex)
+    const bidderContract = Bidder.at(addressFromContractId(bidderContractId))
+    const state = await bidderContract.fetchState()
+    expect(state.asset.alphAmount).toEqual(amount + ONE_ALPH)
+    expect(state.fields.address).toEqual(bidder.address)
+    expect(state.fields.auction).toEqual(auction.contractId)
+  }
+
+  test('bid:update highest bidder', async () => {
+    const [bidder0, bidder1] = bidders
+    await bid(bidder0, auction, alph(10))
+    await checkHighestBidder(bidder0.address, alph(10))
+    await checkBidderState(bidder0, alph(10))
+
+    await bid(bidder1, auction, alph(11))
+    await checkHighestBidder(bidder1.address, alph(11))
+    await checkBidderState(bidder1, alph(11))
+
+    await bid(bidder0, auction, alph(12))
+    await checkHighestBidder(bidder0.address, alph(12))
+    await checkBidderState(bidder0, alph(12))
+
+    await auctionEndFailed(bidder0, auction, Auction.consts.ErrorCodes.AuctionNotYetEnded)
+  })
+
+  test('bid:withdraw', async () => {
+    const [bidder0, bidder1] = bidders
+    await bid(bidder0, auction, alph(10))
+    await checkHighestBidder(bidder0.address, alph(10))
+    await withdrawFailed(bidder0, auction)
+
+    await bid(bidder1, auction, alph(11))
+    await checkHighestBidder(bidder1.address, alph(11))
+    await withdraw(bidder0, auction)
+    await withdrawFailed(bidder1, auction)
+
+    await bid(bidder0, auction, alph(12))
+    await checkHighestBidder(bidder0.address, alph(12))
+
+    await withdraw(bidder1, auction)
+    await checkHighestBidder(bidder0.address, alph(12))
+
+    await auctionEndFailed(bidder0, auction, Auction.consts.ErrorCodes.AuctionNotYetEnded)
+  })
+
+  test('bid:auction end', async () => {
+    const [bidder0, bidder1] = bidders
+    await bid(bidder0, auction, alph(10))
+    await bid(bidder1, auction, alph(11))
+
+    await sleep(11 * 1000)
+
+    await auctionEnd(bidder1, auction)
+    const auctioneerBalance = await balanceOf(ALPH_TOKEN_ID, auctioneer)
+    expect(auctioneerBalance).toEqual(alph(11))
+
+    const beneficiaryAmount = await balanceOf(beneficiaryAsset, bidder1.address)
+    expect(beneficiaryAmount).toEqual(beneficiaryAssetAmount)
+
+    await auctionEndFailed(bidder0, auction, Auction.consts.ErrorCodes.AuctionEndAlreadyCalled)
+
+    await bidFailed(bidder0, auction, alph(12))
+    await withdraw(bidder0, auction)
+  }, 30000)
+})
---
+++ simple-auction/test/utils.ts
@@ -0,0 +1,129 @@
+import { expectAssertionError, testAddress, testPrivateKey } from '@alephium/web3-test'
+import { Auction, AuctionEnd, AuctionInstance, Bid, Bidder, GetToken, TestToken, Withdraw } from '../artifacts/ts'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { ALPH_TOKEN_ID, Address, DUST_AMOUNT, ONE_ALPH, SignerProvider, groupOfAddress, web3 } from '@alephium/web3'
+import { randomBytes } from 'crypto'
+import * as base58 from 'bs58'
+import { waitTxConfirmed as _waitTxConfirmed } from '@alephium/cli'
+
+web3.setCurrentNodeProvider('http://127.0.0.1:22973', undefined, fetch)
+export const ZERO_ADDRESS = 'tgx7VNFoP9DJiFMFgXXtafQZkUvyEdDHT9ryamHJYrjq'
+export const defaultSigner = new PrivateKeyWallet({ privateKey: testPrivateKey })
+
+export async function deployBidderTemplate() {
+  return await Bidder.deploy(defaultSigner, {
+    initialFields: {
+      auction: '',
+      address: ZERO_ADDRESS,
+      bidAmount: 0n
+    }
+  })
+}
+
+export async function deployTestToken(totalSupply: bigint) {
+  const testToken = await TestToken.deploy(defaultSigner, {
+    initialFields: {
+      totalSupply: totalSupply
+    },
+    issueTokenAmount: totalSupply
+  })
+  await GetToken.execute(defaultSigner, {
+    initialFields: {
+      token: testToken.contractInstance.contractId,
+      amount: totalSupply
+    },
+    attoAlphAmount: DUST_AMOUNT
+  })
+  return testToken
+}
+
+export async function deployAuction(auctioneer: Address, auctionEndTime: number) {
+  const bidderTemplate = await deployBidderTemplate()
+  const testTokenAmount = 100n
+  const testToken = await deployTestToken(testTokenAmount)
+
+  return await Auction.deploy(defaultSigner, {
+    initialFields: {
+      bidderTemplateId: bidderTemplate.contractInstance.contractId,
+      auctioneer,
+      beneficiaryAsset: testToken.contractInstance.contractId,
+      beneficiaryAssetAmount: testTokenAmount,
+      auctionEndTime: BigInt(auctionEndTime),
+      highestBidder: ZERO_ADDRESS,
+      highestBid: 0n,
+      ended: false
+    },
+    initialTokenAmounts: [{ id: testToken.contractInstance.contractId, amount: testTokenAmount }]
+  })
+}
+
+export function randomP2PKHAddress(groupIndex = 0): string {
+  const prefix = Buffer.from([0x00])
+  const bytes = Buffer.concat([prefix, randomBytes(32)])
+  const address = base58.encode(bytes)
+  if (groupOfAddress(address) === groupIndex) {
+    return address
+  }
+  return randomP2PKHAddress(groupIndex)
+}
+
+async function waitTxConfirmed<T extends { txId: string }>(promise: Promise<T>): Promise<T> {
+  const result = await promise
+  await _waitTxConfirmed(web3.getCurrentNodeProvider(), result.txId, 1, 1000)
+  return result
+}
+
+export async function transferAlphTo(to: Address, amount: bigint) {
+  return await waitTxConfirmed(
+    defaultSigner.signAndSubmitTransferTx({
+      signerAddress: testAddress,
+      destinations: [{ address: to, attoAlphAmount: amount }]
+    })
+  )
+}
+
+export async function bid(signer: SignerProvider, auction: AuctionInstance, amount: bigint) {
+  return await Bid.execute(signer, {
+    initialFields: { auction: auction.contractId, amount },
+    attoAlphAmount: amount + ONE_ALPH
+  })
+}
+
+export async function bidFailed(signer: SignerProvider, auction: AuctionInstance, amount: bigint) {
+  await expectAssertionError(
+    bid(signer, auction, amount),
+    auction.address,
+    Number(Auction.consts.ErrorCodes.AuctionAlreadyEnded)
+  )
+}
+
+export async function withdraw(signer: SignerProvider, auction: AuctionInstance) {
+  return await Withdraw.execute(signer, { initialFields: { auction: auction.contractId } })
+}
+
+export async function withdrawFailed(signer: SignerProvider, auction: AuctionInstance) {
+  await expectAssertionError(
+    withdraw(signer, auction),
+    auction.address,
+    Number(Auction.consts.ErrorCodes.HighestBidderNotAllowedToWithdraw)
+  )
+}
+
+export async function auctionEnd(signer: SignerProvider, auction: AuctionInstance) {
+  return await AuctionEnd.execute(signer, { initialFields: { auction: auction.contractId } })
+}
+
+export async function auctionEndFailed(signer: SignerProvider, auction: AuctionInstance, errorCode: bigint) {
+  await expectAssertionError(auctionEnd(signer, auction), auction.address, Number(errorCode))
+}
+
+export function alph(amount: bigint | number): bigint {
+  return BigInt(amount) * ONE_ALPH
+}
+
+export async function balanceOf(tokenId: string, address = testAddress): Promise<bigint> {
+  const balances = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(address)
+  if (tokenId === ALPH_TOKEN_ID) return BigInt(balances.balance)
+  const balance = balances.tokenBalances?.find((t) => t.id === tokenId)
+  return balance === undefined ? 0n : BigInt(balance.amount)
+}
