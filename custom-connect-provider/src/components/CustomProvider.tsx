0e5b9f19ac5aff8649851b039c0f84198b6cb6a9
Cheng Wang <wachmc@gmail.com>
2024-06-05 10:03:53 +0200

    Minor

--- custom-connect-provider/src/components/CustomProvider.tsx
+++ custom-connect-provider/src/components/CustomProvider.tsx
@@ -1,9 +1,14 @@
+import React  from 'react'
+
 import { ConnectModal } from './ConnectModal'
 import { AlephiumConnectProvider, ConnectSettingProvider } from '@alephium/web3-react'
 
+const network = 'mainnet'
+const addressGroup: number | undefined = 0
+
 export const CustomProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
-    <AlephiumConnectProvider network='mainnet'>
+    <AlephiumConnectProvider network={network} addressGroup={addressGroup}>
       <ConnectSettingProvider>
         {children}
         <ConnectModal/>
