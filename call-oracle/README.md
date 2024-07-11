c24af582fb197d45d6e5823ec6fc5c0edf8f8223
lbqds <lbqds@outlook.com>
2024-07-11 20:39:47 +0800

    Add an example of calling oracle

---
+++ call-oracle/README.md
@@ -0,0 +1,15 @@
+# Call Oracle 
+
+This example shows how to call the DIA oracle to fetch prices.
+
+# Install
+
+```shell
+npm install
+```
+
+# Run
+
+```shell
+npm run dev
+```
---
+++ call-oracle/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { AlephiumWalletProvider } from '@alephium/web3-react'
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <AlephiumWalletProvider theme="web95" network='testnet' addressGroup={0}>
+      <Component {...pageProps} />
+    </AlephiumWalletProvider>
+  )
+}
