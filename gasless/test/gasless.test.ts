a53c0c93e7bb59ad671436724b9e538d1e46ef3e
h0ngcha0 <lbs.lhc@gmail.com>
2024-11-12 09:00:40 +0100

    Check contract balance

--- gasless/test/gasless.test.ts
+++ gasless/test/gasless.test.ts
@@ -7,23 +7,29 @@ const nodeProvider = web3.getCurrentNodeProvider()
 
 describe('integration tests', () => {
   it('should pay gas fee when needed', async () => {
-    const initialBalance = ONE_ALPH * 100n
-    const [contractDeployer, signer] = await getSigners(2, initialBalance, 0)
+    const initialSignerBalance = ONE_ALPH * 100n
+    const initialContractBalance = ONE_ALPH * 10n
+    const [contractDeployer, signer] = await getSigners(2, initialSignerBalance, 0)
     const { contractInstance } = await Gasless.deploy(
       contractDeployer,
       {
         initialFields: {},
-        initialAttoAlphAmount: ONE_ALPH * 10n
+        initialAttoAlphAmount: initialContractBalance
       }
     )
 
-    await PayNoGas.execute(signer, { initialFields: { gaslessContract: contractInstance.contractId } })
-    const balanceAfterPayNoGas = await nodeProvider.addresses.getAddressesAddressBalance(signer.address)
-    expect(BigInt(balanceAfterPayNoGas.balance)).toBe(initialBalance)
+    const payNoGasTxResult = await PayNoGas.execute(signer, { initialFields: { gaslessContract: contractInstance.contractId } })
+    const signerBalanceAfterPayNoGasTx = await nodeProvider.addresses.getAddressesAddressBalance(signer.address)
+    expect(BigInt(signerBalanceAfterPayNoGasTx.balance)).toBe(initialSignerBalance)
+    const contractBalanceAfterPayNoGasTx = await nodeProvider.addresses.getAddressesAddressBalance(contractInstance.address)
+    const contractGasFee = BigInt(payNoGasTxResult.gasAmount) * BigInt(payNoGasTxResult.gasPrice)
+    expect(BigInt(contractBalanceAfterPayNoGasTx.balance)).toBe(initialContractBalance - contractGasFee)
 
     const payGasResult = await PayGas.execute(signer, { initialFields: { gaslessContract: contractInstance.contractId } })
-    const balanceAfterPayGas = await nodeProvider.addresses.getAddressesAddressBalance(signer.address)
-    const gasFee = BigInt(payGasResult.gasAmount) * BigInt(payGasResult.gasPrice)
-    expect(BigInt(balanceAfterPayGas.balance)).toBe(initialBalance - gasFee)
+    const signerBalanceAfterPayGasTx = await nodeProvider.addresses.getAddressesAddressBalance(signer.address)
+    const signerGasFee = BigInt(payGasResult.gasAmount) * BigInt(payGasResult.gasPrice)
+    expect(BigInt(signerBalanceAfterPayGasTx.balance)).toBe(initialSignerBalance - signerGasFee)
+    const contractBalanceAfterPayGasTx = await nodeProvider.addresses.getAddressesAddressBalance(contractInstance.address)
+    expect(BigInt(contractBalanceAfterPayGasTx.balance)).toBe(initialContractBalance - contractGasFee)
   }, 20000)
 })
