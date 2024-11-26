88f6c0ef23115850db606264e81da99283c06ad5
Ikem Peter <ikempeter2020@gmail.com>
2024-11-26 19:17:00 +0100

    wrote test

---
+++ liquid-staking/test/staking.test.ts
@@ -0,0 +1,168 @@
+import {
+  ALPH_TOKEN_ID,
+  Address,
+  ONE_ALPH,
+  addressFromContractId,
+  groupOfAddress,
+  sleep,
+  subContractId
+} from '@alephium/web3'
+import { Staking, StakingInstance, StakingAccount } from '../artifacts/ts'
+import {
+  deployStaking,
+  stake,
+  stakeFailed,
+  unstake,
+  unstakeFailed,
+  claimRewards,
+  setRewardRate,
+  setRewardRateFailed,
+  balanceOf,
+  randomP2PKHAddress,
+  transferTokenTo,
+  checkStakingAccount,
+  transferAlphTo,
+  alph,
+  updateStartTime
+} from './utils'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { testAddress } from '@alephium/web3-test'
+import * as base58 from 'bs58'
+
+describe('test staking', () => {
+  const groupIndex = groupOfAddress(testAddress)
+  const stakingDuration = 10 * 1000 // 10s
+  const rewardRate = 100n // tokens per second
+  const penaltyPercent = 10n // 10%
+
+  let staking: StakingInstance
+  let owner: Address
+  let tokenId: string
+  let rewardsTokenId: string
+  let stakers: PrivateKeyWallet[]
+  let ownerWallet: PrivateKeyWallet
+
+  beforeEach(async () => {
+    const startTime = Date.now() * 2000
+    ownerWallet = PrivateKeyWallet.Random(groupIndex)
+    owner = ownerWallet.address
+    staking = (await deployStaking(
+      owner,
+      startTime,
+      stakingDuration,
+      penaltyPercent,
+      rewardRate
+    )).contractInstance
+
+    const stakingState = await staking.fetchState()
+    tokenId = stakingState.fields.tokenId
+    rewardsTokenId = stakingState.fields.rewardsTokenId
+
+    stakers = Array.from(Array(2).keys()).map((_) => PrivateKeyWallet.Random(groupIndex))
+    // Setup initial token balances for stakers
+    for (const staker of stakers) {
+      await transferAlphTo(staker.address, alph(1000))
+      await transferTokenTo(staker.address, tokenId, 1000n)
+    }
+
+    await transferAlphTo(owner, alph(1000))
+    await transferTokenTo(owner, tokenId, 1000n)
+  })
+
+  async function checkTotalStaked(expectedAmount: bigint) {
+    const state = await staking.fetchState()
+    expect(state.fields.totalAmountStaked).toEqual(expectedAmount)
+  }
+
+  test('stake:failed scenarios', async () => {
+    const [staker] = stakers
+    
+    // Test staking before pool starts
+    await stakeFailed(
+      staker, 
+      staking, 
+      100n, 
+      tokenId,
+      Number(Staking.consts.ErrorCodes.PoolNotStarted)
+    )
+  })
+
+
+  test('stake:successful staking', async () => {
+    const [staker1, staker2] = stakers
+    const stakeAmount = 100n
+
+    await updateStartTime(staking, ownerWallet)
+
+    // Test initial stake
+    await stake(staker1, staking, stakeAmount, tokenId)
+    // await checkStakingAccount(staking, staker1.address, stakeAmount)
+    await checkTotalStaked(stakeAmount)
+
+    // Test additional stake
+    await stake(staker1, staking, stakeAmount, tokenId)
+    // await checkStakingAccount(staking, staker1.address, stakeAmount * 2n)
+    await checkTotalStaked(stakeAmount * 2n)
+
+    // Test multiple stakers
+    await stake(staker2, staking, stakeAmount, tokenId)
+    // await checkStakingAccount(staking, staker2.address, stakeAmount)
+    await checkTotalStaked(stakeAmount * 3n)
+  })
+
+  test('unstake:with penalty', async () => {
+    const [staker] = stakers
+    const stakeAmount = 100n
+    await updateStartTime(staking, ownerWallet)
+
+    await stake(staker, staking, stakeAmount, tokenId)
+    await unstake(staker, staking, stakeAmount)
+
+    const expectedPenalty = (stakeAmount * penaltyPercent) / 100n
+    const finalBalance = await balanceOf(tokenId, staker.address)
+    expect(finalBalance).toEqual(1000n - expectedPenalty) // Initial 1000n minus penalty
+  })
+
+  test('unstake:after duration', async () => {
+    const [staker] = stakers
+    const stakeAmount = 100n
+    
+    await updateStartTime(staking, ownerWallet)
+    
+    await stake(staker, staking, stakeAmount, tokenId)
+    await sleep(stakingDuration + 1000)
+    await unstake(staker, staking, stakeAmount)
+
+    const finalBalance = await balanceOf(tokenId, staker.address)
+    expect(finalBalance).toEqual(1000n) // Should get full amount back
+  })
+
+  test('rewards:accumulation and claiming', async () => {
+    const [staker] = stakers
+    const stakeAmount = 100n
+    
+    await updateStartTime(staking, ownerWallet)
+
+    await stake(staker, staking, stakeAmount, tokenId)
+    await sleep(3000) // Wait 3 seconds for rewards to accumulate
+
+    const initialRewardsBalance = await balanceOf(rewardsTokenId, staker.address)
+    await claimRewards(staker, staking)
+    const finalRewardsBalance = await balanceOf(rewardsTokenId, staker.address)
+
+    expect(finalRewardsBalance).toBeGreaterThan(initialRewardsBalance)
+  })
+
+  test('admin:reward rate update', async () => {
+    const [nonOwner] = stakers
+    const newRate = 200n
+
+    // Non-owner should fail
+    await setRewardRateFailed(nonOwner, staking, newRate)
+
+    // Owner should succeed
+    await setRewardRate(ownerWallet, staking, newRate)
+    const state = await staking.fetchState()
+    expect(state.fields.rewardRate).toEqual(newRate)
+  })
+})
---
+++ liquid-staking/test/utils.ts
@@ -0,0 +1,289 @@
+import { expectAssertionError, testAddress, testPrivateKey } from '@alephium/web3-test'
+import { 
+  Staking, 
+  StakingInstance, 
+  StakingAccount,
+  TestToken,
+  GetToken,
+  Stake,
+  Unstake,
+  ClaimRewards,
+  SetRewardRate,
+  UpdateStartTime
+} from '../artifacts/ts'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { 
+  ALPH_TOKEN_ID, 
+  Address, 
+  DUST_AMOUNT, 
+  ONE_ALPH, 
+  SignerProvider, 
+  groupOfAddress,
+  web3,
+  waitForTxConfirmation,
+  addressFromContractId,
+  subContractId
+} from '@alephium/web3'
+import { randomBytes } from 'crypto'
+import * as base58 from 'bs58'
+
+web3.setCurrentNodeProvider('http://127.0.0.1:22973', undefined, fetch)
+export const ZERO_ADDRESS = 'tgx7VNFoP9DJiFMFgXXtafQZkUvyEdDHT9ryamHJYrjq'
+export const defaultSigner = new PrivateKeyWallet({ privateKey: testPrivateKey })
+
+async function waitTxConfirmed<T extends { txId: string }>(promise: Promise<T>): Promise<T> {
+  const result = await promise
+  await waitForTxConfirmation(result.txId, 1, 1000)
+  return result
+}
+
+export function randomP2PKHAddress(groupIndex = 0): string {
+  const prefix = Buffer.from([0x00])
+  const bytes = Buffer.concat([prefix, randomBytes(32)])
+  const address = base58.encode(bytes)
+  if (groupOfAddress(address) === groupIndex) {
+    return address
+  }
+  return randomP2PKHAddress(groupIndex)
+}
+
+export async function deployTestToken(totalSupply: bigint) {
+  const testToken = await TestToken.deploy(defaultSigner, {
+    initialFields: {
+      totalSupply: totalSupply
+    },
+    issueTokenAmount: totalSupply
+  })
+  await GetToken.execute(defaultSigner, {
+    initialFields: {
+      token: testToken.contractInstance.contractId,
+      amount: totalSupply
+    },
+    attoAlphAmount: DUST_AMOUNT
+  })
+  return testToken
+}
+
+export async function deployStakingAccountTemplate(tokenId: string, rewardsTokenId: string) {
+  return await StakingAccount.deploy(defaultSigner, {
+    initialFields: {
+      tokenId,
+      rewardsTokenId,
+      staker: ZERO_ADDRESS,
+      parentContractAddress: ZERO_ADDRESS,
+      amountStaked: 0n,
+      rewardPerTokenPaid: 0n,
+      rewards: 0n,
+    }
+  })
+}
+
+export async function deployTestTokens(amount: bigint) {
+  const stakingToken = await deployTestToken(amount)
+  const rewardsToken = await deployTestToken(amount)
+  return { stakingToken, rewardsToken }
+}
+
+export async function deployStaking(
+  owner: Address,
+  startTime: number,
+  duration: number,
+  penaltyPercent: bigint,
+  rewardRate: bigint
+) {
+  const { stakingToken, rewardsToken } = await deployTestTokens(1000000n)
+  
+  const accountTemplate = await deployStakingAccountTemplate(
+    stakingToken.contractInstance.contractId, 
+    rewardsToken.contractInstance.contractId
+  )
+
+  return await Staking.deploy(defaultSigner, {
+    initialFields: {
+      tokenId: stakingToken.contractInstance.contractId,
+      rewardsTokenId: rewardsToken.contractInstance.contractId,
+      stakingAccountTemplateId: accountTemplate.contractInstance.contractId,
+      rewardRate,
+      totalAmountStaked: 0n,
+      rewardPerTokenStored: 0n,
+      lastUpdateTime: 0n,
+      startTime: BigInt(startTime),
+      duration: BigInt(duration),
+      earlyUnstakePenaltyPercent: penaltyPercent,
+      owner_: owner
+    },
+    initialTokenAmounts: [
+      { id: rewardsToken.contractInstance.contractId, amount: 1000000n }
+    ]
+  })
+}
+
+export async function transferTokenTo(to: Address, tokenId: string, amount: bigint) {
+  return await waitTxConfirmed(
+    defaultSigner.signAndSubmitTransferTx({
+      signerAddress: testAddress,
+      destinations: [
+        { 
+          address: to, 
+          attoAlphAmount: ONE_ALPH,
+          tokens: [{ id: tokenId, amount: amount }]
+        }
+      ]
+    })
+  )
+}
+
+
+export async function transferAlphTo(to: Address, amount: bigint) {
+  return await waitTxConfirmed(
+    defaultSigner.signAndSubmitTransferTx({
+      signerAddress: testAddress,
+      destinations: [{ address: to, attoAlphAmount: amount }]
+    })
+  )
+}
+
+export function alph(amount: bigint | number): bigint {
+  return BigInt(amount) * ONE_ALPH
+}
+
+export async function stake(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  amount: bigint,
+  tokenId: string
+) {
+  return await Stake.execute(signer, {
+    initialFields: {
+      staking: staking.contractId,
+      amount
+    },
+    tokens: [{ id: tokenId, amount }],
+    attoAlphAmount: ONE_ALPH
+  })
+}
+
+export async function updateStartTime(staking: StakingInstance, owner: SignerProvider) {
+  return await UpdateStartTime.execute(owner, {
+    initialFields: {
+      staking: staking.contractId
+    }
+  })
+}
+
+export async function stakeFailed(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  amount: bigint,
+  tokenId: string,
+  errorCode: number
+) {
+  await expectAssertionError(
+    stake(signer, staking, amount, tokenId),
+    staking.address,
+    errorCode
+  )
+}
+
+export async function unstake(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  amount: bigint,
+) {
+  return await Unstake.execute(signer, {
+    initialFields: {
+      staking: staking.contractId,
+      amount
+    }
+  })
+}
+
+export async function unstakeFailed(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  amount: bigint,
+  errorCode: number
+) {
+  await expectAssertionError(
+    unstake(signer, staking, amount),
+    staking.address,
+    errorCode
+  )
+}
+
+export async function claimRewards(
+  signer: SignerProvider,
+  staking: StakingInstance
+) {
+  return await ClaimRewards.execute(signer, {
+    initialFields: {
+      staking: staking.contractId
+    }
+  })
+}
+
+export async function claimRewardsFailed(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  errorCode: number
+) {
+  await expectAssertionError(
+    claimRewards(signer, staking),
+    staking.address,
+    errorCode
+  )
+}
+
+export async function setRewardRate(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  rewardRate: bigint
+) {
+  return await SetRewardRate.execute(signer, {
+    initialFields: {
+      staking: staking.contractId,
+      rewardRate
+    }
+  })
+}
+
+export async function setRewardRateFailed(
+  signer: SignerProvider,
+  staking: StakingInstance,
+  rate: bigint
+) {
+  await expectAssertionError(
+    setRewardRate(signer, staking, rate),
+    staking.address,
+    Number(Staking.consts.PermissionsErrorCodes.Forbidden)
+  )
+}
+
+export async function checkStakingAccount(
+  staking: StakingInstance,
+  staker: Address, 
+  expectedAmount: bigint
+) {
+  const groupIndex = groupOfAddress(staker)
+  const path = base58.decode(staker)
+  const accountId = subContractId(staking.contractId, path, groupIndex)
+  const stakingAccount = StakingAccount.at(addressFromContractId(accountId))
+  const state = await stakingAccount.fetchState()
+  expect(state.fields.amountStaked).toEqual(expectedAmount)
+}
+
+export async function balanceOf(tokenId: string, address = testAddress): Promise<bigint> {
+  const balances = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(address)
+  if (tokenId === ALPH_TOKEN_ID) return BigInt(balances.balance)
+  const balance = balances.tokenBalances?.find((t) => t.id === tokenId)
+  return balance === undefined ? 0n : BigInt(balance.amount)
+}
+
+export async function getRewards(staking: StakingInstance, staker: Address): Promise<bigint> {
+  const groupIndex = groupOfAddress(staker)
+  const path = base58.decode(staker)
+  const accountId = subContractId(staking.contractId, path, groupIndex)
+  const stakingAccount = StakingAccount.at(addressFromContractId(accountId))
+  const state = await stakingAccount.fetchState()
+  return state.fields.rewards
+} 
